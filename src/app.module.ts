37c7b33124ade1c73416f42114c8904d4c1fb33a
Neil Ryan L <neilryanlipaod@gmail.com>
2022-03-31 21:49:14 +0800

    Fix database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import databaseConfig from './database/database.config';
 import { UsersModule } from './users/users.module';
 @Module({
-  imports: [UsersModule, AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        playground: Boolean('GRAPHQL__PLAYGROUND'),
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.production'],
+      load: [databaseConfig],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
