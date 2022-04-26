712f24eb8f7658f6d3d97fe0e6e38f27f96a6978
Neil Ryan L <neilryanlipaod@gmail.com>
2022-04-26 22:26:21 +0800

    feat(#4) add streamifier and implement cloudinary service

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import streamifier from 'streamifier';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  async uploadImage(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = v2.uploader.upload_stream(
+        {
+          folder: 'photos',
+          resource_type: 'auto',
+        },
+        (error: Error, result: UploadApiResponse) => {
+          if (result) resolve(result);
+          else reject(error);
+        },
+      );
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
+  }
+}
